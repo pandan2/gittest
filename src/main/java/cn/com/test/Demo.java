a2ef973acab746dd31d799e8f320ad40a6c3a970
pandan2 <zh20161018@163.com>
2020-04-26 18:21:39 +0800

    添加分支

--- src/main/java/cn/com/test/Demo.java
+++ src/main/java/cn/com/test/Demo.java
@@ -4,8 +4,8 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("master分支");
-        System.out.println("新增test分支");
         System.out.println("aaaaaa");
         System.out.println("bbbb");
+        System.out.println("新增test分支");
     }
 }
