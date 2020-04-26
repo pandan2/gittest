ccca164273b62e8fbec98c0ed3152380f55c0438
pandan2 <zh20161018@163.com>
2020-04-26 17:32:22 +0800

    添加aaa

--- src/main/java/cn/com/test/Demo.java
+++ src/main/java/cn/com/test/Demo.java
@@ -5,5 +5,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("master分支");
         System.out.println("新增test分支");
+        System.out.println("aaaaaa");
     }
 }
